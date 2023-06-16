27990d562388456cf5f3ae9b8a8b495ba1f2741d
pcaracal <peter.schreivogel@stud.kbw.ch>
2023-06-16 09:25:43 +0200

    feat: frame for all endpoints using /tasks

--- app.ts
+++ app.ts
@@ -10,9 +10,34 @@ app.use(express.json());
 
 
 
+// GET /tasks: Returns all tasks as JSON and status 200
+app.get('/tasks', async (req: Request, res: Response) => {
+
+});
 
 
+// POST /tasks: Creates a new task and returns itself as JSON and status 201; status 406 if title is empty
+app.post('/tasks', async (req: Request, res: Response) => {
 
+});
+
+
+// GET /tasks/{id}: Returns a single task by its ID as JSON and status 200 or 404
+app.get('/tasks/:id', async (req: Request, res: Response) => {
+
+});
+
+
+// PUT /tasks/{id}: Re-creates a task by its ID and returns new values as JSON and status 200 or 404; status 406 if title is empty
+app.put('/tasks:id', async (req: Request, res: Response) => {
+
+});
+
+
+// DELETE /tasks/{id}: Deletes a task by its ID and status 204 or 404
+app.delete('/tasks:id', async (req: Request, res: Response) => {
+
+});
 
 
 
