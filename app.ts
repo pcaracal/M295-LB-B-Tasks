b4fc21779c7fb06cd1104335b87a27aeb6d64ba1
pcaracal <peter.schreivogel@stud.kbw.ch>
2023-06-16 09:19:20 +0200

    feat: frame for node express app

--- app.ts
+++ app.ts
@@ -1,1 +1,21 @@
-console.log('Hello World');
+import { Application, Request, Response } from 'express';
+import express from 'express';
+const app: Application = express();
+const port = 3000;
+const fs = require('fs');
+const session = require('express-session');
+app.use(express.json());
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(port, () => { console.log(`App is listening to: ${port}`); });
